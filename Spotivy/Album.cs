fabc0b8fb88c6939c34e1ce87bb27ad217951273
StijnR7 <153715745+StijnR7@users.noreply.github.com>
2025-05-08 13:15:58 +0200

    Saved

--- Spotivy/Album.cs
+++ Spotivy/Album.cs
@@ -8,5 +8,12 @@ namespace Spotivy
 {
     internal class Album
     {
+        private string title;
+        private List<Song> songs;
+        private List<Artist> artists;
+
+        public string Title { get => title; set => title = value; }
+        internal List<Song> Songs { get => songs; set => songs = value; }
+        internal List<Artist> Artists { get => artists; set => artists = value; }
     }
 }
--- Spotivy/Artist.cs
+++ Spotivy/Artist.cs
@@ -8,5 +8,12 @@ namespace Spotivy
 {
     internal class Artist
     {
+        private string name;
+        private List<Song> songs;
+        private List<Album> albums;
+
+        public string Name { get => name; set => name = value; }
+        internal List<Song> Songs { get => songs; set => songs = value; }
+        internal List<Album> Albums { get => albums; set => albums = value; }
     }
 }
