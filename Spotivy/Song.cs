fe7c054321cdf88868add6cc863d50975a2ecaeb
StijnR7 <153715745+StijnR7@users.noreply.github.com>
2025-05-08 13:01:00 +0200

    set up classes

---
+++ Spotivy/Song.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Spotivy;
+
+namespace Spotivy
+{
+    internal class Song
+    {
+        private string title;
+        private List<Artist> artists = new();
+        Genre.
+
+    }
+}
--- Spotivy/obj/Debug/net8.0/Spotivy.AssemblyInfo.cs
+++ Spotivy/obj/Debug/net8.0/Spotivy.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Spotivy")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5d7eec81dfc1e8737e3c8a98d4f266718793f51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Spotivy")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Spotivy")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
