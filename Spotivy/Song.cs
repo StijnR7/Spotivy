9395a08455a2d3c59a7d7480fa59b2a2b0d1d6ee
StijnR7 <153715745+StijnR7@users.noreply.github.com>
2025-05-08 13:02:57 +0200

    Added song constructor

--- Spotivy/Song.cs
+++ Spotivy/Song.cs
@@ -11,7 +11,16 @@ namespace Spotivy
     {
         private string title;
         private List<Artist> artists = new();
-        Genre.
+        private Genre genre;
 
+        public Song(string title, List<Artist> artists, Genre genre) {
+            this.title = title;
+            this.artists = artists;
+            this.genre = genre;
+        }
+
+        public string Title { get => title; set => title = value; }
+        public Genre Genre { get => genre; set => genre = value; }
+        internal List<Artist> Artists { get => artists; set => artists = value; }
     }
 }
